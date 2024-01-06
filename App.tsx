3c41816bffa0245d48de3a55e38ba512d998bbd0
Sivasankarcse <75409978+Sivasankarcse@users.noreply.github.com>
2024-01-06 21:02:39 +0530

    Category screen implemented

--- App.tsx
+++ App.tsx
@@ -1,68 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TrackListScreen from './src/screens/TrackListScreen';
-import StatsScreen from './src/screens/StatsScreen';
-import AccountsScreen from './src/screens/AccountsScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import { theme, icons, COLORS } from './constants';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { TabBarIcon } from './src/components/TabBarIcon';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { theme } from './constants';
+import CategoriesScreen from './src/screens/CategoriesScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer theme={theme}>
       <StatusBar style='light' />
-      <Tab.Navigator 
-        // screenOptions={{
-        //   tabBarStyle: {
-        //     backgroundColor: theme.colors.card
-        //   }
-        // }}
-      >
-        <Tab.Screen
-          options={{
-            title: "Date",
-            tabBarActiveTintColor: COLORS?.blue,
-            tabBarInactiveBackgroundColor: theme.colors.card,
-            tabBarIcon: (props) => <TabBarIcon {...props} name='notebook' type='date' />
-          }}
-          name='TrackListScreen'
-          component={TrackListScreen}
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          options={{headerShown: false}}
+          component={HomeScreen}
         />
-        <Tab.Screen
-          options={{
-            title: "Stats",
-            tabBarActiveTintColor: COLORS?.blue,
-            tabBarInactiveBackgroundColor: theme.colors.card,
-            tabBarIcon: (props) => <TabBarIcon {...props} name='stats-chart-outline' type='stats' />
-          }}
-          name='StatsScreen'
-          component={StatsScreen} />
-        <Tab.Screen
-          options={{
-            title: "Accounts",
-            tabBarActiveTintColor: COLORS?.blue,
-            tabBarInactiveBackgroundColor: theme.colors.card,
-            tabBarIcon: (props) => <TabBarIcon {...props} name='switch-account' type='accounts' />
-          }}
-          name='AccountsScreen'
-          component={AccountsScreen} />
-        <Tab.Screen
-          options={{
-            title: "Settings",
-            tabBarActiveTintColor: COLORS?.blue,
-            tabBarInactiveBackgroundColor: theme.colors.card,
-            tabBarIcon: (props) => <TabBarIcon {...props} name="settings" type='settings' />
-          }} 
-          name='SettingsScreen' 
-          component={SettingsScreen} />
-      </Tab.Navigator>
+        <Stack.Screen name='Categories' component={CategoriesScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/screens/CategoriesScreen.tsx
@@ -0,0 +1,127 @@
+import { View, Text, Button, TextInput, KeyboardAvoidingView, TouchableOpacity, Modal } from 'react-native'
+import React, { useState } from 'react'
+import ListItem from '../components/ListItem'
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { ColorPicker, fromHsv } from 'react-native-color-picker'
+
+import { COLORS, theme } from '../../constants';
+import { Category } from '../types/category';
+import CategoryRow from '../components/CategoryRow';
+
+
+const CategoriesScreen = ({ color, name }: Omit<Category, 'id'>) => {
+    const [showColorPicker, setShowColorPicker] = useState(false);
+    const [selectedColor, setSelectedColor] = useState(theme.colors.primary)
+    const [newCategoryName, setNewCategoryName] = useState("");
+
+    const [categories, setCategories] = useState<Category[]>([
+        {
+            id: '1',
+            color: theme.colors.primary,
+            name: 'Groceries',
+        }, {
+            id: '2',
+            color: theme.colors.card,
+            name: 'Clothes'
+        }
+    ]);
+
+    const onSelectColor = ({ hex }) => {
+        setSelectedColor(hex)
+    }
+
+    const createNewCategory = () => {
+        if (newCategoryName.length === 0) {
+            return
+        }
+        setCategories([
+            ...categories,
+            { id: Math.random().toString(), color: selectedColor, name: newCategoryName }
+        ]);
+        setNewCategoryName("")
+        setSelectedColor(theme.colors.primary)
+    }
+
+    return (
+        <>
+            <KeyboardAvoidingView
+                behavior='padding'
+                keyboardVerticalOffset={112}
+                style={{ margin: 16, flex: 1 }}>
+                <View style={{
+                    borderRadius: 11,
+                    overflow: 'hidden'
+                }}>
+                    {categories.map(({ id, name, color }) => (
+                        <CategoryRow key={id} name={name} color={color} id={''} />
+                    ))}
+                </View>
+                <View style={{ flex: 1 }} />
+                <View style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', paddingHorizontal: 8, paddingVertical: 8 }}>
+                    <TouchableOpacity onPress={() => setShowColorPicker(!showColorPicker)}>
+                        <View style={{
+                            backgroundColor: selectedColor,
+                            width: 24,
+                            height: 24,
+                            borderRadius: 12,
+                            borderWidth: 2,
+                            borderColor: COLORS.white
+                        }} />
+                    </TouchableOpacity>
+                    <TextInput
+                        onChangeText={(val) => setNewCategoryName(val)}
+                        value={newCategoryName}
+                        style={{
+                            flex: 1,
+                            color: COLORS.white,
+                            borderColor: theme.colors.border,
+                            borderWidth: 1,
+                            height: 40,
+                            borderRadius: 8,
+                            paddingLeft: 8,
+                            marginLeft: 12,
+                        }}
+                        placeholder='Category name'
+                    />
+                    <TouchableOpacity
+                        onPress={createNewCategory}
+                        style={{ padding: 11 }}
+                    >
+                        <FontAwesome name="send" size={24} color={theme.colors.primary} />
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
+            <Modal 
+                transparent={true} 
+                visible={showColorPicker} 
+                animationType='fade'
+                onRequestClose={() => setShowColorPicker(false)}
+            >
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 24}}>
+                    <View style={{
+                        // flex: 1,
+                        padding: 24,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '100%', 
+                        backgroundColor: theme.colors.card,
+                        borderRadius: 12,
+                        marginTop: 22
+                    }}>
+                        <ColorPicker
+                            hideSliders
+                            color={selectedColor}
+                            onColorChange={(color) => onSelectColor({ hex: fromHsv(color) })}
+                            style={{ width: 200, height: 200 }}
+                        />
+                        <Button onPress={() => setShowColorPicker(false)} title='Select' />
+                    </View>
+                </View>
+            </Modal>
+
+        </>
+    )
+}
+
+export default CategoriesScreen
